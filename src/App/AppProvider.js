c450f14831a1f6433d1f34b57a0320d6814c0998
briantaylor <brian.taylor818@gmail.com>
2019-02-25 02:34:16 -0800

    Adds CryptoCompare API and uses Async/Await to fetch data

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
 
@@ -11,6 +13,16 @@ export class AppProvider extends React.Component {
       setPage: this.setPage,
       confirmFavorites: this.confirmFavorites
     }
+  }
+
+  componentDidMount = () => {
+    this.fetchCoins();
+  }
+
+  fetchCoins = async () => {
+    let coinList = (await cc.coinList()).Data;
+    this.setState({coinList})
+    console.log(coinList);
   }
 
   confirmFavorites = () => {
