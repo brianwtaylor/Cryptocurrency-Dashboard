a1a7098b981d6304bfe3037c21897e6671fba38f
briantaylor <brian.taylor818@gmail.com>
2019-02-25 00:17:12 -0800

    Adds AppLayout component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
+import AppLayout from './AppLayout';
 import styled, {css} from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <WelcomeMessage />
+      <AppLayout>
+        <WelcomeMessage />
+      </AppLayout>
     );
   }
 }
