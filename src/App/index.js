7584b27230c58106520a041f6c96ae974616b34d
briantaylor <brian.taylor818@gmail.com>
2019-02-25 00:30:36 -0800

    Adds AppBar component and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 import styled, {css} from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar />
         <WelcomeMessage />
       </AppLayout>
     );
